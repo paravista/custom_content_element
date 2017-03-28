f336b0100da74b48267dccd9271da9704a00744e
Michael Hitzler <michael.hitzler@paravista.de>
2017-03-28 18:41:46 +0200

    [REFACTOR] change naming of translation ids

--- Configuration/PageTS/CustomContentElements.ts
+++ Configuration/PageTS/CustomContentElements.ts
@@ -7,8 +7,8 @@ mod.wizards.newContentElement.wizardItems.common {
 			icon = EXT:custom_content_element/Resources/Public/Svgs/content-text-image-left.svg
 			#  you can also use one of the existent registered icons via iconIdentifier
 			#iconIdentifier = content-image
-			title = LLL:EXT:custom_content_element/Resources/Private/Language/locallang_db.xlf:TextImageLeft.title
-			description = LLL:EXT:custom_content_element/Resources/Private/Language/locallang_db.xlf:TextImageLeft.description
+			title = LLL:EXT:custom_content_element/Resources/Private/Language/locallang_db.xlf:ce.text_image_left.title
+			description = LLL:EXT:custom_content_element/Resources/Private/Language/locallang_db.xlf:ce.text_image_left.description
 			tt_content_defValues {
 				CType = text_image_left
 			}
