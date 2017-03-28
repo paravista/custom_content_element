26c471f9b3f4d337de9733f14830397a73a5ff85
Michael Hitzler <michael.hitzler@paravista.de>
2017-03-28 18:29:21 +0200

    [TASK] use XLF language specific labels

--- Configuration/PageTS/CustomContentElements.ts
+++ Configuration/PageTS/CustomContentElements.ts
@@ -7,8 +7,8 @@ mod.wizards.newContentElement.wizardItems.common {
 			icon = EXT:custom_content_element/Resources/Public/Svgs/content-text-image-left.svg
 			#  you can also use one of the existent registered icons via iconIdentifier
 			#iconIdentifier = content-image
-			title = Text with image on left
-			description = Text with image placed on left side in 30/70 size
+			title = LLL:EXT:custom_content_element/Resources/Private/Language/locallang_db.xlf:TextImageLeft.title
+			description = LLL:EXT:custom_content_element/Resources/Private/Language/locallang_db.xlf:TextImageLeft.description
 			tt_content_defValues {
 				CType = text_image_left
 			}
