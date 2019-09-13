a4a4a7c177f691f139f05f5b4df27610ac300d32
Michael Hitzler <michael.hitzler@paravista.de>
2019-09-13 20:07:02 +0200

    [BUGFIX] fix page TS file path and set icon from internal sprite

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,6 +1,6 @@
 <?php
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
 	'custom_content_element',
-	'Configuration/TsConfig/Page.tsconfig',
+    'Configuration/PageTS/CustomContentElement.tsconfig',
 	'Custom Content Element Config'
 );
